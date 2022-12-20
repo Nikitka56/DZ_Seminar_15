1f971d42f1ae69d5206615d1d8a4262cf972fdd7
Nikitka56 <119408380+Nikitka56@users.noreply.github.com>
2022-12-20 11:02:53 +0700

    Add files via upload

---
+++ DZ_Seminer_ 15/Zadacha_64/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Task2
+{
+    class Test
+    {
+        static int SumNumbers(int M, int N)
+        {
+            if (M == 0) return (N * (N + 1)) / 2;            // Если M равно нулю
+            else if (N == 0) return (M * (M + 1)) / 2;       // Если N равно нулю
+            else if (M == N) return M;                       // Если M=N
+            else if (M < N) return N + SumNumbers(M, N - 1); // Если M<N
+            else return N + SumNumbers(M, N + 1);            // Если M>N
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("M = ");
+            int M = int.Parse(Console.ReadLine());
+            Console.Write("N = ");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Result, S = {SumNumbers(M, N)}");
+            Console.ReadLine();
+        }
+    }
+}
